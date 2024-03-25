d8aa077f8a8de932176a6d2fde91e790df403721
jugol-kumar <jk23717933@gmail.com>
2024-03-25 19:12:04 +0600

    change window size

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
     <section>
         <div class="container-fluid">
             <div class="row">
-                <div id="carouselExampleDark" class="carousel slide">
+                <div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
                             aria-current="true" aria-label="Slide 1"></button>
@@ -13,20 +13,20 @@
                             aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
-                        <div class="carousel-item active" data-bs-interval="10000">
+                        <div class="carousel-item active" data-bs-interval="5000">
                             <RouterLink to="">
                                 <img src="https://relaxtheback.com/cdn/shop/files/GiftsOfComfortAndJoy-DesktopBanner_HOLIDAY_2023_1_2588x771.jpg?v=1703010563"
                                     class="d-block w-100" alt="..." />
                             </RouterLink>
                         </div>
-                        <div class="carousel-item" data-bs-interval="2000">
+                        <div class="carousel-item" data-bs-interval="5000">
                             <RouterLink to="">
                                 <img src="https://relaxtheback.com/cdn/shop/files/Gravis_Save_500_Desktop_Homepage_Banner_2023_83c52d95-9199-4a2c-b2e6-fd76495c96e3_1348x400.png?v=1698875670"
                                     class="d-block w-100" alt="..." />
                             </RouterLink>
 
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item" data-bs-interval="5000">
                             <RouterLink to="">
                                 <img src="https://relaxtheback.com/cdn/shop/files/Qi_XE_Pro_Save_500_Desktop_Banner_HOLIDAY_2023_1_1348x400.png?v=1699299403"
                                     class="d-block w-100" alt="..." />
@@ -70,7 +70,7 @@
         <div class="container-fluid">
             <div class="new-arrivals">
                 <RouterLink to="" class="new-arrivals__content">
-                    <img :src="`${$API_URL}${category?.photo}`" alt="">
+                    <img :src="`${$API_URL}/storage/${category?.photo}`" alt="">
                     <div class="new-arrivals__content__text">
                         <h2>{{ category?.name }}</h2>
                         <p>This season give the gift of wellness. A gift that offers relief, recovery, or relaxation for
